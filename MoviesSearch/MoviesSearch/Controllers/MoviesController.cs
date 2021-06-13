c1fec46d8fa06861762748351d82b1b67879810e
quyentho <thokimquangquyen@gmail.com>
2021-06-13 20:45:31 +0700

    basic query on title and story line

--- MoviesSearch/MoviesSearch/Controllers/MoviesController.cs
+++ MoviesSearch/MoviesSearch/Controllers/MoviesController.cs
@@ -20,9 +20,26 @@ namespace MoviesSearch.Controllers
         }
 
         // GET: MoviesController
-        public ActionResult Search()
+        public ActionResult Search(string queryString)
         {
             var result = _client.Search<Movie>(s => s);
+            if (!string.IsNullOrEmpty(queryString))
+            {
+                result = _client.Search<Movie>(s => s
+                    .Size(25)
+                    .Query(q => q
+                        .MultiMatch(m => m
+                            .Fields(f => f
+                                .Field(p => p.Title.Suffix("keyword"), 1.5)
+                                .Field(p => p.Title, 1.5)
+                                .Field(p => p.Storyline, 0.8)
+                            )
+                            .Query(queryString)
+                        )
+                    )
+                );
+            }
+
             var model = new SearchViewModel
             {
                 Hits = result.Hits,
