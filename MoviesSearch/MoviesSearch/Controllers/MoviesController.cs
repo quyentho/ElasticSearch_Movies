a093c724dee5fc9f52197b979cafc7c1457ec0c2
quyentho <thokimquangquyen@gmail.com>
2021-06-13 23:02:10 +0700

    remove unused code

--- MoviesSearch/MoviesSearch/Controllers/MoviesController.cs
+++ MoviesSearch/MoviesSearch/Controllers/MoviesController.cs
@@ -1,12 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using MoviesSearch.Domain;
 using MoviesSearch.Models;
 using Nest;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MoviesSearch.Controllers
 {
@@ -47,37 +42,9 @@ namespace MoviesSearch.Controllers
                         .Query(queryString)
                         )
 
-
-                    
-                    //    .Query(query => query.MultiMatch(
-                    //        m => m.Fields(
-                    //            f => f
-                    //        .Field(p => p.Countries.Suffix("keyword"), 500)
-                    //        .Field(p => p.Cast.Suffix("keyword"), 100)
-                    //        .Field(p => p.Directors.Suffix("keyword"), 100)
-                    //        .Field(p => p.Languages.Suffix("keyword"), 100)
-                    //        .Field(p => p.Writers.Suffix("keyword"), 100)
-                    //            )
-                    //        ))
                     )
                     );
             }
-            /*
-             * .Field(p => p.Countries.Suffix("keyword"), 500)
-                                .Field(p => p.Cast.Suffix("keyword"), 100)
-                                .Field(p => p.Directors.Suffix("keyword"), 100)
-                                .Field(p => p.Languages.Suffix("keyword"), 100)
-                                .Field(p => p.Writers.Suffix("keyword"), 100)
-            */
-
-
-            //.Field(p => p.Title, 1.5)
-            //                    .Field(p => p.Storyline, 0.8)
-            //                    .Field(p => p.Cast, 1.5)
-            //                    .Field(p => p.Countries, 1.5)
-            //                    .Field(p => p.Directors, 1.5)
-            //                    .Field(p => p.Writers, 1.5)
-
             var model = new SearchViewModel
             {
                 Hits = result.Hits,
